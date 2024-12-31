6214607cdf3f615865ec4be4d0696265d1968558
Jesse Batman <jesse.batman@outlook.com>
2024-12-31 02:03:59 +0200

    Fix pom.xml & include pipeline for backend

--- backend/src/main/java/fi/morabotti/ipclassify/security/ApplicationUser.java
+++ backend/src/main/java/fi/morabotti/ipclassify/security/ApplicationUser.java
@@ -1,10 +1,14 @@
 package fi.morabotti.ipclassify.security;
 
 import io.jsonwebtoken.Claims;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ApplicationUser {
     private Long id = 0L;
